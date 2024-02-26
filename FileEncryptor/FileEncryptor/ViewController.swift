e4ad43488562da8b656f84b237f233b4d92d4cf7
ZDBryukhanov <zdbryukhanov@mai.education>
2024-02-27 02:02:55 +0300

    init MacOS project

---
+++ FileEncryptor/FileEncryptor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FileEncryptor
+//
+//  Created by Захар Брюханов on 27.02.2024.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
