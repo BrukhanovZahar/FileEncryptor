39cc2e22a3ce55594b9392ecccc577df83a39265
Daniil <danast_2@mail.ru>
2024-03-03 06:21:38 +0300

    update_commit_1.5(little changes in example realisation)

--- C++/example.cpp
+++ C++/example.cpp
@@ -1,9 +1,13 @@
 #include "library.h"
 
 int main(){
-    std::string path_to_notcrypt = "/Users/danast/Desktop/encryption/notcrypt.jpg";
-    std::string path_to_willBeEncr = "/Users/danast/Desktop/encryption/notcrypt_encrypted.jpg";
-    //std::string path_to_decrypt = "/Users/danast/Desktop/encryption/decrypt.txt";
+    //std::string path_to_notcrypt = "/Users/danast/Desktop/encryption/notcrypt.jpg";
+    std::string path_to_notcrypt;
+    std::cout << "Enter path to not crypt file: "; std::cin >> path_to_notcrypt;
+
+    //std::string path_to_Encr = "/Users/danast/Desktop/encryption/notcrypt_encrypted.jpg";
+    std::string path_to_encr;
+    std::cout << "Enter path to crypt file: "; std::cin >> path_to_encr;
 
     //для простоты, ключ и IV будут одинаковыми
     const CryptoPP::byte* IV = EncryptionLib::generate_IV();
@@ -22,6 +26,6 @@ int main(){
 
     EncryptionLib::encrypt_file(path_to_notcrypt, KEY, IV);
 
-    EncryptionLib::decrypt_file(path_to_willBeEncr, KEY, IV);
+    EncryptionLib::decrypt_file(path_to_encr, KEY, IV);
     return 0;
 }
