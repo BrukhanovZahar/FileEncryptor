144117602b7f7f6e1926a26288ca63de156d5602
ZDBryukhanov <zdbryukhanov@mai.education>
2024-03-11 02:16:17 +0300

    add 2 text field and constraint

--- FileEncryptor/FileEncryptor/ViewController.swift
+++ FileEncryptor/FileEncryptor/ViewController.swift
@@ -8,11 +8,54 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    var textField1: NSTextField!
+    var textField2: NSTextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        configureTextField1()
+        configureTextField2()
+    }
+    
+    private func configureTextField1() {
+        textField1 = NSTextField()
+        view.addSubview(textField1)
+        
+        textField1.translatesAutoresizingMaskIntoConstraints = false
+        textField1.textColor = .white
+        textField1.placeholderString = "Введите абсолютный путь до файла"
+        textField1.alignment = .center
+        
+        
+        NSLayoutConstraint.activate([
+            textField1.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5, constant: 0),
+            textField1.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.08, constant: 0),
+            textField1.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            textField1.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -20)
+        ])
+    }
+    
+    private func configureTextField2() {
+        textField2 = NSTextField()
+        view.addSubview(textField2)
+        
+        textField2.translatesAutoresizingMaskIntoConstraints = false
+        textField2.textColor = .white
+        textField2.placeholderString = "Введите ключ"
+        textField2.alignment = .center
+        
+        NSLayoutConstraint.activate([
+            textField2.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.5, constant: 0),
+            textField2.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.08, constant: 0),
+            textField2.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            textField2.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: 20)
+        ])
     }
 
     override var representedObject: Any? {
